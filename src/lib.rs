d18c576593b883a144d0f744cecaf38d1023f719
SusanHex <suehex4027@gmail.com>
2023-01-28 22:36:07 -0700

    SpotifyApplication struct and new method

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+struct SpotifyApplication {
+    client_id: String,
+    response_type: String,
+    redirect_uri: String,
+    scope: Option<String>,
+}
+
+impl SpotifyApplication {
+    pub fn new(
+        client_id: String,
+        redirect_uri: String,
+        scope: Option<String>,
+    ) -> SpotifyApplication {
+        SpotifyApplication {
+            client_id: client_id,
+            response_type: "code".to_owned(),
+            redirect_uri: redirect_uri,
+            scope: scope,
+        }
+    }
+}
